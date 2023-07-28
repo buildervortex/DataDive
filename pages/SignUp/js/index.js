f0839975d57a8cf3ee9838b69fa4431dd1d3d966
DGNuwan <dgnuwanthilakarathna@gmail.com>
2023-07-28 15:51:33 +0530

    profile and signup page last update

--- pages/SignUp/js/index.js
+++ pages/SignUp/js/index.js
@@ -1,4 +1,9 @@
 import { setDescriptionTextArea } from "../../../../js/other/limitedDescription/index.js";
 
+import { InitializeTheSocialMediaLinkAdder } from "../../../../js/other/socialMediaLinkList/index.js";
+
 // add the character limit default is 500
 setDescriptionTextArea(500);
+
+
+InitializeTheSocialMediaLinkAdder();
