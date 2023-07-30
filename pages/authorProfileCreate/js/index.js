1b3f15a0dfa2a1e3538cde1d8a9616de0c4287d1
kasun <hklrupasingha@gmail.com>
2023-07-30 19:28:14 +0530

    authur profile create page

---
+++ pages/authorProfileCreate/js/index.js
@@ -0,0 +1,7 @@
+import {navBarInit} from "../../../../js/other/navBar/navBar.js";
+import {InitializeTheSocialMediaLinkAdder} from "../../../js/other/socialMediaLinkList/index.js";
+navBarInit();
+
+InitializeTheSocialMediaLinkAdder();
+
+
