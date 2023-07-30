745b80ee5d7d3c2c957990cc5b634162f6629be5
Janith Panduka Weerathunga <140271432+GKJPW@users.noreply.github.com>
2023-07-30 18:38:49 +0530

    Homepage & Footer all completed

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 
     <link rel="stylesheet" href="./css/css/main/heading/pageHead.css" />
     <link rel="stylesheet" href="./css/other/carousel/carousel.css" />
+    <link rel="stylesheet" href="./css/other/footer/footer.css"/>
 
 </head>
 
@@ -38,7 +39,7 @@
     <main>
         <header class="pageHead">
             <div class="container">
-                <h1>Find the right freelance service, right away</h1>
+                <h1>"Empower Humanity With Your Knowledge"</h1>
                 <div class="searchBar">
                     <form action="">
                         <input type="text" placeholder="Search" />
@@ -51,58 +52,111 @@
             <img src="./icon/other/carousel/leftArrow.png" id="leftArrow" class="carosalArrows">
             <div class="carosalContainer">
                 <div class="cards">
-                    <img src="./img/other/carousel/cardimage.jpeg"/>
-                    <h1>heading1</h1>
-                    <div class="description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus architecto sequi atque perferendis, tenetur, exercitationem ducimus vitae veniam fugiat numquam sint earum accusamus quis illo nam, asperiores et? Architecto, iste.</div>
+                    <img src="./img/other/carousel/information.jpg"/>
+                    <h1>Information Technology</h1>
+                    <div class="description"><b>Data Revolution:</b> Pushing the Posibilities of Cyber Environment, over the Imaginary Probability by Innovating, Transforming & Elevating the existing uses of Information & Resources. </div>
                     <button>Click</button>
                 </div>
                 <div class="cards">
-                    <img src="./img/other/carousel/cardimage.jpeg"/>
-                    <h1>heading2</h1>
-                    <div class="description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus architecto sequi atque perferendis, tenetur, exercitationem ducimus vitae veniam fugiat numquam sint earum accusamus quis illo nam, asperiores et? Architecto, iste.</div>
+                    <img src="./img/other/carousel/science.jpg"/>
+                    <h1>Science with Human-Being </h1>
+                    <div class="description"><b>Pursuit of Truth:</b> Cruising through the Universe of Mysteries & Expanding the Vision of Everything by Exploring new Theories & Uncovering new Dimentions to put a Forward Step as a whole Humanity.</div>
                     <button>Click</button>
                 </div>
                 <div class="cards">
-                    <img src="./img/other/carousel/cardimage.jpeg"/>
-                    <h1>heading4</h1>
-                    <div class="description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus architecto sequi atque perferendis, tenetur, exercitationem ducimus vitae veniam fugiat numquam sint earum accusamus quis illo nam, asperiores et? Architecto, iste.</div>
+                    <img src="./img/other/carousel/maths.jpg"/>
+                    <h1>Mathematics & Statistical Logics</h1>
+                    <div class="description"><b>Symphony of Equations:</b> Indicating the ways of  Unlocking the Gates of Patterns & Possibilities by the Keys of Creativity & Precision to Uncover Numerical Secrets</div>
                     <button>Click</button>
                 </div>
                 <div class="cards">
-                    <img src="./img/other/carousel/cardimage.jpeg"/>
-                    <h1>heading5</h1>
-                    <div class="description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus architecto sequi atque perferendis, tenetur, exercitationem ducimus vitae veniam fugiat numquam sint earum accusamus quis illo nam, asperiores et? Architecto, iste.</div>
+                    <img src="./img/other/carousel/graphic.jpg"/>
+                    <h1>Graphics with Digitalism</h1>
+                    <div class="description"><b>Creating Imaginary Realties:</b> Embracing the World of Digital-Arts that Pushes the Pixels to Perfect Combinations</div>
                     <button>Click</button>
                 </div>
                 <div class="cards">
-                    <img src="./img/other/carousel/cardimage.jpeg"/>
-                    <h1>heading6</h1>
-                    <div class="description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus architecto sequi atque perferendis, tenetur, exercitationem ducimus vitae veniam fugiat numquam sint earum accusamus quis illo nam, asperiores et? Architecto, iste.</div>
+                    <img src="./img/other/carousel/business.png"/>
+                    <h1>Business & Enterpreneurship</h1>
+                    <div class="description"><b>Venturing into Success:</b> Observing the Dynamics of Economic Interraction & Empower the Innovative Mind to build-up from Startup to Success</div>
                     <button>Click</button>
                 </div>
                 <div class="cards">
-                    <img src="./img/other/carousel/cardimage.jpeg"/>
-                    <h1>heading7</h1>
-                    <div class="description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus architecto sequi atque perferendis, tenetur, exercitationem ducimus vitae veniam fugiat numquam sint earum accusamus quis illo nam, asperiores et? Architecto, iste.</div>
+                    <img src="./img/other/carousel/language.png"/>
+                    <h1>Languages, Culture & Diversity</h1>
+                    <div class="description"><b>Beyond Borders:</b> Breaking the Barriers of Comphrehension, Expression, Literation & Communication to Strengthen the Bonds of Humanity.</div>
                     <button>Click</button>
                 </div>
                 <div class="cards">
-                    <img src="./img/other/carousel/cardimage.jpeg"/>
-                    <h1>heading8</h1>
-                    <div class="description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus architecto sequi atque perferendis, tenetur, exercitationem ducimus vitae veniam fugiat numquam sint earum accusamus quis illo nam, asperiores et? Architecto, iste.</div>
+                    <img src="./img/other/carousel/art.jpg"/>
+                    <h1>Art with Creativity</h1>
+                    <div class="description"><b>Unbound Imagination:</b> Exploring the Depths of Artistic Expression by A Language Without Boundaries, Spoken by the Heart that expresses The Emotive Power of Visual Art.</div>
                     <button>Click</button>
                 </div>
                 <div class="cards">
-                    <img src="./img/other/carousel/cardimage.jpeg"/>
-                    <h1>heading9</h1>
-                    <div class="description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus architecto sequi atque perferendis, tenetur, exercitationem ducimus vitae veniam fugiat numquam sint earum accusamus quis illo nam, asperiores et? Architecto, iste.</div>
+                    <img src="./img/other/carousel/music.jpg"/>
+                    <h1>Rhythm & Music</h1>
+                    <div class="description"><b>Heartbeat of Art:</b> Perfect Combination of  Tones, Melodies and Vocals in Perfect Dose, that's the core of a Rhythmic SoundWave which gives Supreme Sensation.</div>
+                    <button>Click</button>
+                </div>
+                <div class="cards">
+                    <img src="./img/other/carousel/interests.jpg"/>
+                    <h1>Lifestyle & Interests</h1>
+                    <div class="description"><b>Art of Living:</b> Harmonizing Health, Work and Pleasure in day-to-day Routine by Finding New Methods of Living with Joy and Making the Best Choices on Every Instance.</div>
                     <button>Click</button>
                 </div>
             </div>
             <img src="./icon/other/carousel/rightArrow.png" id="rightArrow" class="carosalArrows">
         </section>
     </main>
-    <footer></footer>
+    
+    <footer class="footer">
+        <div class="container">
+         <div class="row">
+           <div class="footer-col">
+             <h4>Website</h4>
+             <ul>
+               <li><a href="#">Explore</a></li>
+               <li><a href="#">About Us</a></li>
+               <li><a href="#">Services</a></li>
+               <li><a href="#">Help</a></li>
+             </ul>
+           </div>
+           <div class="footer-col">
+             <h4>Operations</h4>
+             <ul>
+               <li><a href="#">Create Account</a></li>
+               <li><a href="#">User Login</a></li>
+               <li><a href="#">Category Requests</a></li>
+               <li><a href="#">Report a Problem</a></li>
+             </ul>
+           </div>
+           <div class="footer-col">
+             <h4>Quick Links</h4>
+             <ul>
+               <li><a href="#">User Profile</a></li>
+               <li><a href="#">Publish</a></li>
+               <li><a href="#">Your Posts</a></li>
+               <li><a href="#">Instructions </a></li>
+             </ul>
+           </div>
+           <div class="footer-col">
+             <h4>Get Connected</h4>
+             <div class="social-links">
+                <a href="https://www.gmail.com"><i class="fab fa-email"><img src="../../../icon/other/footer/icons8-email-67.png" height="40cqh" width="40cqh"></i></a>
+                <a href="https://www.github.com"><i class="fab fa-github"><img src="../../../icon/other/footer/icons8-github-64.png"height="40cqh" width="40cqh"></i></a>
+                <a href="https://www.facebook.com"><i class="fab fa-facebook-f"><img src="../../../icon/other/footer/icons8-facebook-50.png" height="40cqh" width="40cqh"></i></a>
+                <a href="https://www.linkedin.com"><i class="fab fa-linkedin-in"><img src="../../../icon/other/footer/icons8-linked-in-50.png" height="40cqh" width="40cqh"></i></a>
+                <div>
+                    <br><p id="bbv">#BinaryBuilderVorteX</p>
+                    <p id="bbv">#TeamEffort @ 2023</p>
+                </div>
+             </div>
+           </div>
+         </div>
+        </div>
+     </footer>
+    
     <script type="module" src="./js/js/index.js"></script>
 </body>
 
