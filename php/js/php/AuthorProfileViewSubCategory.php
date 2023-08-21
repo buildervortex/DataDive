a7ce2d897d393a02c1212f31e78a08c3549ce097
galahirudilhara <galahirudilhara@gmail.com>
2023-08-21 15:39:18 +0530

    recover a deleted file

---
+++ php/js/php/AuthorProfileViewSubCategory.php
@@ -0,0 +1,18 @@
+<?php
+$DOCUEMENT_ROOT = $_SERVER["DOCUMENT_ROOT"];
+include_once $DOCUEMENT_ROOT."/php/lib/db/pages/AuthorProfileView/AuthorProfileViewDatabaseHandler.php";
+
+
+function subcategory($mainCategory){
+    return json_encode(getSubCategory(isCookiesThere(),$mainCategory),JSON_FORCE_OBJECT);
+}
+
+$decodeJsonData;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['CONTENT_TYPE'] === 'application/json') {
+    $jsonData = file_get_contents('php://input');
+    $decodeJsonData = json_decode($jsonData);
+}
+
+$subCategoryJson = subcategory($decodeJsonData->category,$Category);
+echo $subCategoryJson;
